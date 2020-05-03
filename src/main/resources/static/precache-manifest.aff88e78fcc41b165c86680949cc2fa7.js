3f68bfdc420daed2f3281d05be2c827f2d0f262a
pranshu <uhsnarpggupta@gmail.com>
2020-05-04 03:55:19 +0530

    Added static frontend files

---
+++ src/main/resources/static/precache-manifest.aff88e78fcc41b165c86680949cc2fa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3279880027e27f9f9b24e3f0b2dfcb3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cace50eff5682e2eeb4e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4fd4b1c0c3aa3fedecf9",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "cace50eff5682e2eeb4e",
+    "url": "/static/js/2.a470b880.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a470b880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd4b1c0c3aa3fedecf9",
+    "url": "/static/js/main.8d528797.chunk.js"
+  },
+  {
+    "revision": "af5bb9d9a47cefba8622",
+    "url": "/static/js/runtime-main.b5a87144.js"
+  }
+]);
